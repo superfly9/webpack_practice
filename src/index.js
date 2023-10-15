ea54344b2e7dd35936eeb71e756f0657864be0b0
송승찬 <superfly09@daum.net>
2023-10-15 17:31:51 +0900

    feat: entry/ouput 없는 설정 추가 - mode:development만 추가 => dist/main.js는 압축이 안 된 상태

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import { add } from "./math.js";
+import { add, MathCalculator, multiply } from "./math.js";
 
-console.log(add(2, 3));
+/* multiply main.js에 포함되지 X 
+=> export 대상을 실제로 호출해서 써야 main.js에 포함
+=> MathCalculator의 minus는 실제 호출은 안 하지만, 클래스를 import 해오면서 같이 딸려오기에 main.js에 포함
+ */
+const obj = new MathCalculator(10, 20, -5);
+obj.add();
+
+console.log(add(500, 220));
--- src/math.js
+++ src/math.js
@@ -1,1 +1,25 @@
-export const add = (a, b) => a + b;
+const add = (a, b) => a + b;
+const multiply = (a, b) => a * b;
+
+class MathCalculator {
+  constructor(...numbers) {
+    this.numbers = numbers;
+    this.currentValue = 0;
+  }
+  add(currentValue) {
+    this.currentValue = this.numbers.reduce(
+      (acc, cur) => acc + cur,
+      currentValue || this.currentValue
+    );
+  }
+  minus(currentValue) {
+    this.currentValue = this.numbers.reduce(
+      (acc, cur) => acc - cur,
+      currentValue || this.currentValue
+    );
+  }
+}
+
+export { MathCalculator, add, multiply };
+
+// index.js에서 class.minus는 호출 X지만 main.js에는 포함
