21cd971a088cce1b975a15da798dd9410c195915
송승찬 <superfly09@daum.net>
2023-10-22 13:13:14 +0900

    feat: plugin 추가 - Banner Plugin 사용 - 빌드 결과물에 빌드된 시점 남기기 위한 용도 - loader 없이 실행시킴

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,23 @@
+const webpack = require("webpack"); //to access built-in plugins
+const path = require("path");
+
 module.exports = {
   mode: "development",
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [{}],
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: (yourVariable) => {
+        return `Banner Plugnin Test ${new Date().toLocaleString()}`;
+      },
+    }),
+  ],
 };
