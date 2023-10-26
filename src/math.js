893de0bdb24a640044178b0433fe66f57453224b
송승찬 <superfly09@daum.net>
2023-10-26 20:16:55 +0900

    feat: babel.config.js 없이 웹팩 로더에 추가 - 이렇게만 해도 webpack 실행시, babel 잘 돌아간다

--- src/math.js
+++ src/math.js
@@ -23,3 +23,4 @@ class MathCalculator {
 export { MathCalculator, add, multiply };
 
 // index.js에서 class.minus는 호출 X지만 main.js에는 포함
+//node node_modules/.bin/babel src/math.js --presets @babel/preset-env와 같이 실행도 가능
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,14 @@
+const path = require("path");
 module.exports = {
   mode: "development",
+  entry: {
+    main: "/src/index.js",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(process.cwd(), "dist"),
+  },
+  module: {
+    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }],
+  },
 };
