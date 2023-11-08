a99d4521fd9b644f35a91e70aafd15ec5ad45687
송승찬 <superfly09@daum.net>
2023-11-08 16:47:06 +0900

    feat: prod/ dev 설정 분리 - prod: style 파일 따로 분리( js 번들에 포함시키지 않고,css파일도 병렬 로드위해) / code minify / devServer 필요 X - dev:  style 파일 분리X / js 번들에 포함되어 있다(따라서 파일 용량이 크다) / code minify : X / devServer 사용

---
+++ webpack.prod.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    // assetModuleFilename: "images/[hash][ext]",
+  },
+  devServer: {
+    port: 9000,
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        ],
+      },
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] },
+      {
+        // 파일을 생성해주는 역할
+        test: /\.(jpg|png)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[hash][ext]",
+        },
+      },
+      {
+        test: /\.svg$/,
+        type: "asset/inline",
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+    new HtmlWebpackPlugin({
+      title: "Hello_PROD_ENV",
+      description: "prod_configuration_webpack",
+    }),
+  ],
+};
